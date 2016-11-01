a7a5660d0510787790304b3d449196db303f13f3
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2016-10-31 22:56:02 -0400

    Tweak image widening JS to compensate for 1px border

--- assets/js/widen-images.js
+++ assets/js/widen-images.js
@@ -3,7 +3,7 @@
 	var images = document.querySelectorAll('#post-body p > img');
 	for (var i = 0; i < images.length; i++) {
 		// Only apply to wide images
-		if ( images[i].clientWidth < 520 ) {
+		if ( images[i].clientWidth < 518 ) {
 			return;
 		}
 		var wrap = images[i].parentNode;
