87b98857501d728f3eaf3c30539a54ed12f87beb
micahjon <micahjon@users.noreply.github.com>
2020-11-30 01:50:18 +0000

    deploy: fa0f3d0f73d1a971598a5c17c403865a2ed2dc30

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,33 @@
-// To make images retina, add a class "2x" to the img element
-// and add a <image-name>@2x.png image. Assumes jquery is loaded.
- 
-function isRetina() {
-	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
-					  (min--moz-device-pixel-ratio: 1.5),\
-					  (-o-min-device-pixel-ratio: 3/2),\
-					  (min-resolution: 1.5dppx)";
- 
-	if (window.devicePixelRatio > 1)
-		return true;
- 
-	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
-		return true;
- 
-	return false;
-};
- 
- 
-function retina() {
-	
-	if (!isRetina())
-		return;
-	
-	$("img.2x").map(function(i, image) {
-		
-		var path = $(image).attr("src");
-		
-		path = path.replace(".png", "@2x.png");
-		path = path.replace(".jpg", "@2x.jpg");
-		
-		$(image).attr("src", path);
-	});
-};
- 
-$(document).ready(retina);
+!(function () {
+  'use strict'
+
+  // Increase width of images on large screens
+  var images = document.querySelectorAll('.post__body p > img')
+  for (var i = 0; i < images.length; i++) {
+    // Only apply to wide images
+    if (images[i].clientWidth >= 518) {
+      var wrap = images[i].parentNode
+      if (wrap.nodeName === 'P') {
+        wrap.classList.add('post__wide-image')
+      }
+    }
+  }
+
+  // Don't cut off long lines of code
+  var codes = document.querySelectorAll('.post__body pre[class^="language-"]')
+  for (var i = 0; i < codes.length; i++) {
+    var maxLineLength = codes[i].innerHTML
+      .split('<br>')
+      .map((line) =>
+        line
+          .replace(/<[^>]+>/g, '') // Remove invisible HTML tags
+          .replace(/&[a-z0-9]{2,4};/g, '$') // Replace HTML entities w/ a single character
+          .trim()
+      )
+      .filter(Boolean)
+      .reduce((maxLength, line) => Math.max(maxLength, line.length), 0)
+    if (maxLineLength > 62) {
+      codes[i].classList.add('post__wide-pre')
+    }
+  }
+})()
